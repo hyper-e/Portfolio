0f83bdce2d4528ff6233fedb5ed8b98263998bd5
eduardo reta rj <er031991@gmail.com>
2019-09-01 08:44:59 -0400

    worked on the server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const app = express();
-const router = require("express").Router();
-const PORT = process.env.PORT || 3001;
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
 
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 
-router.use(function(request, response) {
-    response.sendFile(path.join(__dirname, "../client/build/index.html"));
-})
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function() {
-    console.log(`🌎 ==> Server now listening on PORT ${PORT}!`);
-})
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
