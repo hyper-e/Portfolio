a16fa1f4c23f957ba25229c118a01443d8fc38dd
eduardo reta rj <https://github.com/Eddie-Reta>
2020-07-13 23:48:56 -0400

    working on mobile view

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,29 +1,79 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import "./CSS/navStyle.css";
+import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/Button";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Navbar from "react-bootstrap/Navbar";
 class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { matches: window.matchMedia("(min-width: 800px)").matches};
+  }
 
-  handleEmailClick = () => {
-    window.location.href = 'mailto:dev.eddie.reta@gmail.com';
-};
+  componentDidMount() {
+    const handler = e => this.setState({matches: e.matches});
+    window.matchMedia("(min-width: 800px)").addListener(handler);
+  }
 
-    render(){
+  // handlePageLink = () => {
+  //   this.setState
+  // };
+
+  render() {
     return (
-      <nav className="navBar">
-         <div className="name">Eduardo Reta</div>
-          <div className="linkBox">
-              <NavLink to="/" className="buttonLink">About</NavLink>
-              <span className="seperator">|</span>
-              <NavLink to="/portfolio" className="buttonLink">Portfolio</NavLink>
-              <span className="seperator">|</span>
-              <NavLink to="/resume" className="buttonLink">Resume</NavLink>
-              <span className="seperator">|</span>
-              <NavLink to="/contact" onClick={this.handleEmailClick} className="buttonLink">Contact</NavLink>
+      <div >
+      {this.state.matches && (
+        <nav>
+         <div className="navBar">
+           <div className="name">Eduardo Reta</div>
+           <div className="linkBox">
+             <NavLink to="/" className="buttonLink">
+               About
+             </NavLink>
+             <span className="seperator">|</span>
+             <NavLink to="/portfolio" className="buttonLink">
+               Portfolio
+             </NavLink>
+             <span className="seperator">|</span>
+             <NavLink to="/resume" className="buttonLink">
+               Resume
+             </NavLink>
+             <span className="seperator">|</span>
+             <NavLink
+              to="/contact"
+              className="buttonLink"
+            >
+              Contact
+            </NavLink>
           </div>
-      </nav>
+        </div>
+        </nav>
+      )}
+      {!this.state.matches && (
+         <nav className="navTwo">
+            <div className="collapseNav">
+              <div><h3 className="navTwoName">Eduardo Reta</h3></div>
+               <Dropdown>
+                 <Dropdown.Toggle
+                  variant="success"
+                  id="dropdown-basic-button"
+                ></Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="/">Home</Dropdown.Item>
+                  <Dropdown.Item href="/portfolio">Portfolio</Dropdown.Item>
+                  <Dropdown.Item href="/resume">Resume</Dropdown.Item>
+                  <Dropdown.Item href="/contact">Contact</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+            </nav>
+      )}
+      </div>
     );
   }
 }
-  
-  export default Nav;
-  
+
+export default Nav;
--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -4,17 +4,22 @@ import Links from "../components/Links";
 import Mail from "./images/mail.png"
 import Nav from "../components/Nav";
 class Contact extends Component {
+   
+    handleEmailClick = () => {
+        window.location.href = "mailto:dev.eddie.reta@gmail.com";
+      };
+
     render() {
         return(
             
             <div className="contactPage">    
             <div className="navBarFix"><Nav /></div>           
                 {/* <div className="mailImage"><img src={Mail} alt="Mail" className="mailPic" /></div> */}
-                <div className="emailMessage">Click the <strong>Contact</strong> link up top 
+                <div className="emailMessage">Click my <strong>Email</strong> 
                 <br />
                 to get in touch!
                 <br/> 
-                Email: <span className="emailLink">dev.eddie.reta@gmail.com</span>
+                Email: <span className="emailLink" onClick={this.handleEmailClick}>dev.eddie.reta@gmail.com</span>
 
                 </div>
                 <Links linkBox="contactLinks" />
