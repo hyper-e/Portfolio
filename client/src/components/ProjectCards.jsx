e89b02a55e8ddcb7ed605eab891db16c82aa90bf
eduardo reta rj <https://github.com/Eddie-Reta>
2021-02-22 15:58:52 -0500

    Added new prjects to portfolio.

--- client/src/components/ProjectCards.jsx
+++ client/src/components/ProjectCards.jsx
@@ -1,90 +1,114 @@
 import React, { Component } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub} from "@fortawesome/free-brands-svg-icons";
-import CrystalBall from "./images/crystalBallImg.png";
-import Crystals from "./images/crystalImg.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import NodeStack from "./images/NoteStack.jpg";
+import Liri from "./images/Liri.jpg";
 import Gif from "./images/gifs.gif";
 import Dice from "./images/dice.jpg";
 import "./CSS/cardStyle.css";
 
-
 class ProjectCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { image: [] }
-    };
-  
-    componentDidMount() {
-      this.handleGitLink();
+  constructor(props) {
+    super(props);
+    this.state = { image: [], links: false };
+  }
+
+  componentDidMount() {
+    this.handleGitLink();
+    this.missingLink();
+  }
+  handleGitLink = () => {
+    // window.location.href = this.props.github
+    // console.log(this.props.image)
+    if (this.props.image === "NodeStack") {
+      this.setState({ image: NodeStack });
     }
-    handleGitLink = () => {
-        // window.location.href = this.props.github
-        // console.log(this.props.image)
-        if (this.props.image === "CrystalBall") {
-          this.setState({image: CrystalBall});
-        };
-        if (this.props.image === "Crystals") {
-          this.setState({image: Crystals});
-        };
-        if (this.props.image === "Gifs") {
-          this.setState({image: Gif});
-        };
-        if (this.props.image === "Dice") {
-          this.setState({image: Dice});
-        };
-        // if (this.props.image === "Click") {
-        //   this.setState({image: Click});
-        // };
-        // if (this.props.image === "Dog") {
-        //   this.setState({image: Dog});
-        // };
-        // if (this.props.image === "Loop") {
-        //   this.setState({image: Loop});
-        // };
-        // if (this.props.image === "Box") {
-        //   this.setState({image: Box});
-        // };
-    };
-    
-    render() {
-      const titleStyle = {
-        color: "#222731",
-      };
-     
-        return(
-            <div className="card">
-      <div className="img-container">
-        <img className="imageControl" src={this.state.image} alt={this.props.name} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong style={titleStyle}>Project:</strong> {this.props.name}
-          </li>
-          <li>
-              <strong style={titleStyle}>Description:</strong> {this.props.description}
-          </li>
-          <li>
-          <strong style={titleStyle}>Technologies:</strong> {this.props.technologies}
-          </li>
-          <li>
-            <button className="herokuButton" onClick={(link) => {
-                link = this.props.link
-                window.open(link, "_blank");
-            }}>Website Link</button>
-          </li>
-          <li>
-           <button className="githubButton" onClick={(link) => {
-               link = this.props.github
-               window.open(link, "_blank");}} >
-                   <FontAwesomeIcon icon={faGithub} size="2x"/></button>
-          </li>
-        </ul>
-      </div>
-    </div>
+    if (this.props.image === "Liri") {
+      this.setState({ image: Liri });
+    }
+    if (this.props.image === "Gifs") {
+      this.setState({ image: Gif });
+    }
+    if (this.props.image === "Dice") {
+      this.setState({ image: Dice });
+    }
+    // if (this.props.image === "Click") {
+    //   this.setState({image: Click});
+    // };
+    // if (this.props.image === "Dog") {
+    //   this.setState({image: Dog});
+    // };
+    // if (this.props.image === "Loop") {
+    //   this.setState({image: Loop});
+    // };
+    // if (this.props.image === "Box") {
+    //   this.setState({image: Box});
+    // };
+  };
 
-        );
-    };
-};
+  missingLink = () => {
+    var link = this.props.link;
+    if (link != undefined || link != null) {
+      this.setState({ links: true });
+    }
+  };
 
-export default ProjectCard; 
+  render() {
+    const titleStyle = {
+      color: "#222731",
+    };
+
+    return (
+      <div className="card">
+        <div className="img-container">
+          <img
+            className="imageControl"
+            src={this.state.image}
+            alt={this.props.imageCredit}
+          />
+        </div>
+        <div className="content">
+          <ul>
+            <li>
+              <strong style={titleStyle}>Project:</strong> {this.props.name}
+            </li>
+            <li>
+              <strong style={titleStyle}>Description:</strong>{" "}
+              {this.props.description}
+            </li>
+            <li>
+              <strong style={titleStyle}>Technologies:</strong>{" "}
+              {this.props.technologies}
+            </li>
+            {this.state.links && (
+              <li>
+                <button
+                  className="herokuButton"
+                  onClick={(link) => {
+                    link = this.props.link;
+                    window.open(link, "_blank");
+                  }}
+                >
+                  Website Link
+                </button>
+              </li>
+            )}
+            <li>
+              <button
+                className="githubButton"
+                onClick={(link) => {
+                  link = this.props.github;
+                  window.open(link, "_blank");
+                }}
+              >
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProjectCard;
