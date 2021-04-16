920f131a05d5616d43e42999fb4404e8a507cb7c
eduardo reta rj <https://github.com/Eddie-Reta>
2021-04-16 00:49:16 -0400

    Emailjs plugin in html file.

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,3 @@
-import {init} from "emailjs-com";
-init( process.env.REACT_APP_USER_ID || process.env.USER_ID);
-
 function Pass() {
     const keys = {
         serviceId : (process.env.REACT_APP_SERVICE_ID || process.env.SERVICE_ID),
