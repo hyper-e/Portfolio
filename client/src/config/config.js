6db2aa13b48cd86fa46af7515658fb02473a5c81
eduardo reta rj <https://github.com/Eddie-Reta>
2021-04-15 23:32:44 -0400

    Added config keys using heroku.

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,36 +1,11 @@
-const axios = require("axios");
 
-axios({
-    method: "get",
-    url: "https://api.heroku.com/apps/eddie-reta/config-vars",
-    headers:{"Accept" : "application/vnd.heroku+json; version=3"},
-})
-.then(function(response) {
-    console.log(response)
-})
-.catch(function(error){
-    console.log("this is the " + error)
-})
+function Pass() {
+    const keys = {
+        serviceId : (process.env.REACT_APP_SERVICE_ID || process.env.SERVICE_ID),
+        templateId : (process.env.REACT_APP_TEMPLATE_ID || process.env.TEMPLATE_ID),
+        userId : (process.env.REACT_APP_USER_ID || process.env.USER_ID)
+    };
+    return keys;
+}
 
-
-
-
-// const aws = require('aws-sdk');cd
-
-// let s3 = new aws.S3({
-//   accessKeyId: process.env.SERVICE_ID,
-//   secretAccessKey: process.env.S3_SECRET
-// });
-
-// console.log(s3.accessKeyId)
-
-// function Pass() {
-//     const keys = {
-//         serviceId : process.env.REACT_APP_SERVICE_ID,
-//         templateId : process.env.REACT_APP_TEMPLATE_ID,
-//         userId : process.env.REACT_APP_USER_ID
-//     };
-//     return keys;
-// }
-
-//export default Pass;
+export default Pass;
