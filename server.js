628ab561f650229dc29a5bf2e44829b8148840d8
hyper-e <er031991@gmail.com>
2021-04-19 16:04:13 -0400

    .env file heroku

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-console.log(process.env.PORT)
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
