b53f972c2a3a9f15039e69a5017bb2866cdf345a
Eduardo Reta Jr <https://github.com/Eddie-Reta>
2022-04-16 17:16:46 -0400

    package.json server side.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ class App extends Component {
                         
                     <Switch>
                     <Route exact path={"/"} component={Home} />
+                    <Route exact path={"/api"} />
                     <Route component={ NoMatch } />
                     </Switch>
                   
