800ed77fb2470c80670744b29d09e1e01b0d5ef5
Eduardo Reta Jr <https://github.com/Eddie-Reta>
2022-04-16 16:34:25 -0400

    Server changes and package.json changes for POST request.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 const path = require("path");
 const cors = require("cors");
 const app = express();
@@ -10,30 +10,40 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 
-app.use(cors())
-
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json({limit: "1mb"}));
 
-app
-.get("*", function(req, res) {
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.post("/api", (req, res) => {
+  console.log(req.body)
+})
+
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 })
-.post("/api", (req, res) => {
-//  email api
-  const templateParams = {
-    user_name: req.body.user_name,
-    message: req.body.message,
-    user_email: req.body.user_email
-  };
+// app
+// .get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "client/build", "index.html"));
+// })
+// .post("/api", (req, res) => {
+// //  email api
+//   const templateParams = {
+//     user_name: req.body.user_name,
+//     message: req.body.message,
+//     user_email: req.body.user_email
+//   };
  
  
-emailjs.send(process.env.SERVICE_ID, process.env.TEMPLATE_ID, templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-      if (error) { console.log('FAILED...', error) };
-    });
-});
+// emailjs.send(process.env.SERVICE_ID, process.env.TEMPLATE_ID, templateParams)
+//     .then(function(response) {
+//        console.log('SUCCESS!', response.status, response.text);
+//     }, function(error) {
+//       if (error) { console.log('FAILED...', error) };
+//     });
+// });
 
 
 //email api
