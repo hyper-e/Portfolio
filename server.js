9232cd1b5a58361fdd0b182c458ebb32d5e18d3b
Eduardo Reta Jr <dev.eddie.reta@gmail.com>
2022-09-05 00:47:23 -0400

    ls

--- server.js
+++ server.js
@@ -10,32 +10,41 @@ const emailjs = require("emailjs-com");
 //   app.use(express.static(path.join(__dirname, "./client/build/index.html")));
 // }
 
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json({limit: "1mb"}));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/api/test", (req, res) => {
+  res.send("test");
+});
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.join(__dirname, './client/build')));
 
-app.post("/api", (req, res) => {
-//  email api
-console.log(req.body)
-  const templateParams = {
-    user_name: req.body.user_name,
-    message: req.body.message,
-    user_email: req.body.user_email
-  };
- emailjs.send(process.env.SERVICE_ID, process.env.TEMPLATE_ID, templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-      if (error) { console.log('FAILED...', error) };
-    });
-})
+// app.post("/api/email", (req, res) => {
+// //  email api
+// console.log(req.body)
+//   const templateParams = {
+//     user_name: req.body.user_name,
+//     message: req.body.message,
+//     user_email: req.body.user_email
+//   };
+//  emailjs.send(process.env.SERVICE_ID, process.env.TEMPLATE_ID, templateParams)
+//     .then(function(response) {
+//        console.log('SUCCESS!', response.status, response.text);
+//     }, function(error) {
+//       if (error) { console.log('FAILED...', error) };
+//     });
+// })
 
 app.get("*", function(req, res) {
   res.sendFile(
-    path.join(__dirname, "./client/build/index.html")
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
     );
 })
 // app
