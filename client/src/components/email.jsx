c77d92d49c020477e383b6946db9b743067da610
Eduardo Reta Jr <dev.eddie.reta@gmail.com>
2022-09-19 15:57:06 -0400

    email jsx function added placeholder unit email env.variables work on vercel.

--- client/src/components/email.jsx
+++ client/src/components/email.jsx
@@ -1,141 +1,155 @@
-import React, { useState, useRef } from "react";
-import Links from "./Links";
-import emailjs from "emailjs-com";
+import React from "react";
+// import Links from "./Links";
+// import emailjs from "emailjs-com";
 
 
-const SERVICE_ID = process.env.REACT_APP_SERVICE_ID || process.env.SERVICE_ID; 
-const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID || process.env.TEMPLATE_ID;
-const USER_ID = process.env.REACT_APP_USER_ID || process.env.USER_ID;
+// const SERVICE_ID = process.env.SERVICE_ID || process.env.REACT_APP_SERVICE_ID; 
+// const TEMPLATE_ID = process.env.TEMPLATE_ID || process.env.REACT_APP_TEMPLATE_ID;
+// const USER_ID = process.env.USER_ID || process.env.REACT_APP_USER_ID;
 
  function Email() {  
-  const form = useRef();
-  const [emailInfo, setEmailInfo] = useState({
-    user_name: "",
-    message: "",
-    user_email: "",
-  });
-  const [contactInfo, setContactInfo] = useState({
-    name: false,
-    message: false,
-    email: false,
-  });
-
-  const emailReady = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID)
-      .then((result) => {
-          console.log(`Status: ${result.status} \n Status: ${result.text} \n Message: Thank you for reaching out!`);
-      }, (error) => {
-          console.log(error.text);
-      });
-
-    setContactInfo({
-      name: false,
-      message: false,
-      email: false,
-    });
-    setEmailInfo({
-      user_name: "",
-      message: "",
-      user_email: "",
-    });
-  };
-
-  const insertInfo = {
-    needInfo: (x) => {
-      setContactInfo((preValue) => {
-        return {
-          ...preValue,
-          [x]: true,
-        };
-      });
-    },
-    completeInfo: (x, y, z) => {
-      setContactInfo(() => {
-        return {
-          [x]: false,
-          [y]: false,
-          [z]: false,
-        };
-      });
-    },
-  };
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-    if (
-      emailInfo.user_name === "" ||
-      emailInfo.message === "" ||
-      emailInfo.user_email === ""
-    ) {
-      setContactInfo({
-        name: true,
-        message: true,
-        email: true,
-      });
-    } else {
-      insertInfo.completeInfo("name", "message", "email");
-      emailReady(e);
-    }
-  };
-
-  const handleChange = (event) => {
-    const { value, name } = event.target;
-    setEmailInfo((preValue) => {
-      return {
-        ...preValue,
-        [name]: value,
-      };
-    });
-  };
-
+  //EMAIL PLACEHOLDER 
   return (
-    <div className="emailForm">
-      <form ref={form} className="contactForm" onSubmit={sendEmail}>
-        <p className="formPara">Fill out the form to get in contact.</p>
-        <label className="formInfo">
-          Name:
-          <input
-            className={contactInfo.name ? "conName formIncomplete" : "conName"}
-            type="text"
-            name="user_name"
-            value={emailInfo.user_name}
-            placeholder={contactInfo.name ? "Please add Name" : null}
-            onChange={handleChange}
-          />
-        </label>
-        <label className="formInfo">Message: </label>
-        <textarea
-          className={
-            contactInfo.message ? "conMessage messageIncomplete" : "conMessage"
-          }
-          type="text"
-          name="message"
-          placeholder={contactInfo.message ? "Please add Message" : null}
-          value={emailInfo.message}
-          onChange={handleChange}
-        />
-        <label className="formInfo">
-          Email:
-          <input
-            className={
-              contactInfo.email ? "conEmail formIncomplete" : "conEmail"
-            }
-            type="email"
-            name="user_email"
-            value={emailInfo.user_email}
-            placeholder={contactInfo.email ? "Please add Email" : null}
-            onChange={handleChange}
-          />
-        </label>
-        <button className="emailFormButton" type="submit" value="send">
-          <strong>Send</strong>
-        </button>
-      </form>
-      <Links linkBox="contactLinks" />
+    <div>
+      <h1 className="emailPlaceholder">Thank you for checking out my site.</h1>
+      <h2 className="emailPlaceholder">To checkout my resume out, please click on the resume button link.</h2>
     </div>
-  );
-}
+  )
+ }
+
+  ///////////////////////////////////////////////////////////////////////////////
+  ///// WORKING CODE FOR EMAIL.JS.
+  ///CUT OUT BECAUSE COULDN'T SETUP EVIORMENT VARIABLES WITH VERCEL DUE TO BUILDING MY OWN BUILD FOLDER.
+  ////////////////////////////////////////////////////////////////////////////////
+
+  // const form = useRef();
+  // const [emailInfo, setEmailInfo] = useState({
+  //   user_name: "",
+  //   message: "",
+  //   user_email: "",
+  // });
+  // const [contactInfo, setContactInfo] = useState({
+  //   name: false,
+  //   message: false,
+  //   email: false,
+  // });
+
+  // const emailReady = (e) => {
+  //   e.preventDefault();
+
+  //   emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID)
+  //     .then((result) => {
+  //         console.log(`Status: ${result.status} \n Status: ${result.text} \n Message: Thank you for reaching out!`);
+  //     }, (error) => {
+  //         console.log(error.text);
+  //     });
+
+  //   setContactInfo({
+  //     name: false,
+  //     message: false,
+  //     email: false,
+  //   });
+  //   setEmailInfo({
+  //     user_name: "",
+  //     message: "",
+  //     user_email: "",
+  //   });
+  // };
+
+  // const insertInfo = {
+  //   needInfo: (x) => {
+  //     setContactInfo((preValue) => {
+  //       return {
+  //         ...preValue,
+  //         [x]: true,
+  //       };
+  //     });
+  //   },
+  //   completeInfo: (x, y, z) => {
+  //     setContactInfo(() => {
+  //       return {
+  //         [x]: false,
+  //         [y]: false,
+  //         [z]: false,
+  //       };
+  //     });
+  //   },
+  // };
+
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+  //   if (
+  //     emailInfo.user_name === "" ||
+  //     emailInfo.message === "" ||
+  //     emailInfo.user_email === ""
+  //   ) {
+  //     setContactInfo({
+  //       name: true,
+  //       message: true,
+  //       email: true,
+  //     });
+  //   } else {
+  //     insertInfo.completeInfo("name", "message", "email");
+  //     emailReady(e);
+  //   }
+  // };
+
+  // const handleChange = (event) => {
+  //   const { value, name } = event.target;
+  //   setEmailInfo((preValue) => {
+  //     return {
+  //       ...preValue,
+  //       [name]: value,
+  //     };
+  //   });
+  // };
+
+  // return (
+  //   <div className="emailForm">
+  //     <form ref={form} className="contactForm" onSubmit={sendEmail}>
+  //       <p className="formPara">Fill out the form to get in contact.</p>
+  //       <label className="formInfo">
+  //         Name:
+  //         <input
+  //           className={contactInfo.name ? "conName formIncomplete" : "conName"}
+  //           type="text"
+  //           name="user_name"
+  //           value={emailInfo.user_name}
+  //           placeholder={contactInfo.name ? "Please add Name" : null}
+  //           onChange={handleChange}
+  //         />
+  //       </label>
+  //       <label className="formInfo">Message: </label>
+  //       <textarea
+  //         className={
+  //           contactInfo.message ? "conMessage messageIncomplete" : "conMessage"
+  //         }
+  //         type="text"
+  //         name="message"
+  //         placeholder={contactInfo.message ? "Please add Message" : null}
+  //         value={emailInfo.message}
+  //         onChange={handleChange}
+  //       />
+  //       <label className="formInfo">
+  //         Email:
+  //         <input
+  //           className={
+  //             contactInfo.email ? "conEmail formIncomplete" : "conEmail"
+  //           }
+  //           type="email"
+  //           name="user_email"
+  //           value={emailInfo.user_email}
+  //           placeholder={contactInfo.email ? "Please add Email" : null}
+  //           onChange={handleChange}
+  //         />
+  //       </label>
+  //       <button className="emailFormButton" type="submit" value="send">
+  //         <strong>Send</strong>
+  //       </button>
+  //     </form>
+  //     <Links linkBox="contactLinks" />
+  //   </div>
+  //);
+//}
 
 export default Email;
